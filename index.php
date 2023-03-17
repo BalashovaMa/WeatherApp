b13f9588857e0b427017b81991d0f3bbf704de4d
BalashovaMa <balashova.m.nd@gmail.com>
2023-03-17 11:54:50 +0200

    refactor index.php

--- index.php
+++ index.php
@@ -1,38 +1,93 @@
-<?php 
-    if(isset($_GET['city'])) {
-        $urlContent = file_get_contents('https://api.openweathermap.org/data/2.5/weather?q=London&appid=24079dfe2fa15a31fd23b31138cb89f7');
-        $forcastArray = json_decode($urlContent, true);
-        print_r($forcastArray);
-       
-    }
+<?php
+
+error_reporting(E_ERROR | E_PARSE);
+
+$weather = "";
+
+$error = "";
+
+if (isset($_GET['city'])) {
+
+    $urlContent = file_get_contents('https://api.openweathermap.org/data/2.5/weather?q=' . $_GET['city'] . '&units=metric&appid=e26fbcf1d6e728a5a2bd51c9a23b2dcd');
+
+    $forcastArray = json_decode($urlContent, true);
+
+    $weather = 'The weather in ' . $_GET['city'] . ' is ' . $forcastArray['weather'][0]['description'];
+    $weather = $weather.'. The temperature is '.$forcastArray['main']['temp'];
+
+
+
+
+
+}
+
 ?>
+
 
 <!doctype html>
 <html lang="en">
 
 <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+
     <title>Weather App</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="./styles/style.css">
 </head>
 
 <body>
-    <div class="container" id='mainDiv'>
-        <h1>Weather In Your City</h1>
+
+
+    <div class="container" id="mainDiv">
+        <h1>Weather In The City</h1>
+
         <form>
-            <div class="mb-3">
-                <label for="city" class="form-label">Input a city name</label>
-                <input class="form-control" id="city" aria-describedby="Forcast city" placeholder="Enter city name" name='city'>
+            <div class="form-group">
+                <label for="city">Input a city name</label>
+                <input class="form-control" id="city" name="city" aria-describedby="Forcast city"
+                    placeholder="Enter city name">
             </div>
 
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+
+    <div id="forecastDiv">
+
+
+
+        <?php
+
+        if ($weather) {
+
+            echo '<div class="alert alert-primary" role="alert">' . $weather . '</div>';
+
+        } else if ($error) {
+            echo '<div class="alert alert-danger" role="alert">' . $error . '</div>';
+        }
+
+        ?>
+
+
+
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
         crossorigin="anonymous"></script>
 </body>
 
